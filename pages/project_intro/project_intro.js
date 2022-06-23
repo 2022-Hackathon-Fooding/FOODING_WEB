cb0555133fdf2c5d8b602041507afe0e20c10f2d
White-Patrasche <5338095@gmail.com>
2022-06-24 07:36:09 +0900

    project_intro done , header modified, chat list

---
+++ pages/project_intro/project_intro.js
@@ -0,0 +1,6 @@
+function intro_form() {
+  document.getElementsByClassName('desc_wraper')[0].style.display = 'none';
+  document.getElementsByClassName('team_wraper')[0].style.display = 'block';
+  document.body.style.backgroundColor = '#FFFEF5';
+}
+// intro_form();
